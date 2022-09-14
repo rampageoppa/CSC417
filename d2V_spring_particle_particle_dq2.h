bda038b444926441f3150932ff7defdf5b24cf80
rampageoppa <90885941+rampageoppa@users.noreply.github.com>
2022-09-14 12:31:03 -0400

    Delete d2V_spring_particle_particle_dq2.h

--- d2V_spring_particle_particle_dq2.h
+++
@@ -1,9 +0,0 @@
-
-#include <Eigen/Dense>
-
-//Input:
-//  q - the current generalized coordinates for the mass-spring system
-//  stiffness - the stiffness parameter (spring constant) for the mass-spring system
-//Output:
-// H - the second derivtive of the potential energy
-void d2V_spring_particle_particle_dq2(Eigen::MatrixXd &H, const Eigen::VectorXd &q, double stiffness);
